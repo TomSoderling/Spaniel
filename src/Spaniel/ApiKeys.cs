3e00124cb6cbd95e54f2da23b2443d1068ed133e
Tom Soderling <hey_tommy@icloud.com>
2017-12-30 15:53:45 -0600

    Added dummy class for app center values

---
+++ src/Spaniel/ApiKeys.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Spaniel
+{
+    public static class ApiKeys
+    {
+        public const string AppCenterAppSecret_iOS = "[your iOS App Center secret goes here]";
+        public const string AppCenterAppSecret_Android = "[your Android App Center secret goes here]";
+    }
+}
