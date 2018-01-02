441a0f7221d70755d691ed15f523abc0c9426a04
Tom Soderling <hey_tommy@icloud.com>
2018-01-01 19:59:47 -0600

    Added intent filter and logic to import .spaniel files

--- src/Droid/MainActivity.cs
+++ src/Droid/MainActivity.cs
@@ -1,47 +1,99 @@
 ﻿using Android.App;
+using Android.Content;
 using Android.Content.PM;
 using Android.OS;
 using System;
 using Xamarin;
 using Spaniel.Shared;
+using System.IO;
+using System.Xml.Linq;
+using Xamarin.Forms;
+using Spaniel.Shared.Services;
 
 namespace Spaniel.Droid
 {
     [Activity(Label = "Spaniel", Icon = "@drawable/icn_launcher", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [IntentFilter(new[] { Intent.ActionSend },
+                  Categories = new[] { Intent.CategoryDefault },
+                  DataMimeType = "text/xml",
+                  DataPathPattern = "*.spaniel")] // not sure this does what I want it to
+                  //DataScheme = "*.spaniel")] // not sure what this does
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
     {
         protected override void OnCreate(Bundle bundle)
         {
-            try
-            {
-                //#if DEBUG
-                //// Use Insights debug mode.  Any unhandled crash reports, native crash reports, and any Insights.Track() or Insights.Identify() calls will be ignored.
-                //    Insights.Initialize(Insights.DebugModeKey, Application.Context); 
-                //#else
-                //    Insights.Initialize(Constants.InsightsApiKey, Application.Context);
-                //#endif
-            }
-            catch(Exception ex)
-            {
-                // eat this exception. don't let insights initialization crash the app.  #paranoid
-                Console.WriteLine("Exception caught. Xamarin Insights failed on Initialize() call. Exception message: " + ex.Message);
-            }
-
-
-
             base.OnCreate(bundle);
 
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
 
             // map the StyleID to the navtive controls.  This is for Xamarin.UITest
-            global::Xamarin.Forms.Forms.ViewInitialized += (sender, e) => 
+            global::Xamarin.Forms.Forms.ViewInitialized += (sender, e) =>
                 {
                     if (!string.IsNullOrWhiteSpace(e.View.StyleId))
                         e.NativeView.ContentDescription = e.View.StyleId;
                 };
 
             LoadApplication(new App());
+
+
+            // remove the ridiculous app icon on the action bar
+            this.ActionBar.SetIcon(Android.Resource.Color.Transparent);
+
+
+            if (Intent.Action == Intent.ActionSend)
+            {
+                // This is just an example of the data stored in the extras 
+                //var uriFromExtras = Intent.GetParcelableExtra(Android.Content.Intent.ExtraStream) as Android.Net.Uri;
+                //var subject = Intent.GetStringExtra(Android.Content.Intent.ExtraSubject);
+
+                // Get the info from ClipData 
+                var spanielFile = Intent.ClipData.GetItemAt(0);
+
+                // Open a stream from the URI 
+                var stream = ContentResolver.OpenInputStream(spanielFile.Uri);
+
+                // Save it over 
+                var memoryStream = new MemoryStream();
+                stream.CopyTo(memoryStream);
+
+                var docsPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+                var filePath = Path.Combine(docsPath, "incoming.spaniel"); // Saves the file to: /data/user/0/com.TomSoderling.Spaniel/files/incoming.spaniel
+
+                File.WriteAllBytes(filePath, memoryStream.ToArray());
+                memoryStream.Dispose();
+
+
+                try
+                {
+                    // Import the projects from the file
+                    var fileData = File.ReadAllText(filePath);
+
+                    var element = XElement.Parse(fileData);
+
+                    var projRepo = new ProjectRepository();
+                    var projectsDoc = projRepo.LoadProjectFileFromDevice(); // load main in projects.xml file from device
+
+                    if (projectsDoc != null)
+                    {
+                        projectsDoc.Root.Add(element); // add this new node to the projects.xml file
+                        projRepo.SaveProjectsDocument(projectsDoc); // save back to device
+
+                        // Send message to tell the ProjectViewModel to reload the Project List
+                        MessagingCenter.Send<IProjectRepository>(projRepo, "Reload");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    AlertDialog.Builder dialog = new AlertDialog.Builder(this);  
+                    AlertDialog alert = dialog.Create();  
+                    alert.SetTitle("Error Parsing Spaniel File");  
+                    alert.SetMessage("Message: " + ex.Message);
+                    alert.SetButton("Rats", (sender, e) => { }); 
+                    alert.Show();  
+                }
+
+            }
         }
     }
 }
--- src/Droid/Services/ShareService_Droid.cs
+++ src/Droid/Services/ShareService_Droid.cs
@@ -4,6 +4,7 @@ using Xamarin.Forms;
 using Spaniel.Droid.Services;
 using Android.Content;
 using System.IO;
+using Android.Webkit;
 
 [assembly: Dependency(typeof(ShareServiceDroid))]
 
@@ -11,34 +12,48 @@ namespace Spaniel.Droid.Services
 {
     public class ShareServiceDroid : IShareService
     {
+        readonly Context localContext = Android.App.Application.Context;
+
         public void Share(string xmlData, string filename, string projectName)
         {
             var documents = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
             var fullFilename = Path.Combine(documents, filename);
-            File.WriteAllText(fullFilename, xmlData);
 
             // TODO: is there a better way to get a file than writing to device?
+            File.WriteAllText(fullFilename, xmlData);
+
 
             var projectFile = new Java.IO.File(fullFilename);
             var uri = Android.Net.Uri.FromFile(projectFile);
             projectFile.SetReadable(true, false);
 
-            var myIntent = new Intent(Intent.ActionSend); // Also known as the "share" intent, you should use this in an intent with startActivity() when you have some data that the user can share through another app, such as an email app or social sharing app.
-            myIntent.SetType("text/plain");       // this allows the user to pick email or bluetooth (which could be nice). Don't need to lock them into Email only
-            //myIntent.SetType("message/rfc822"); // this causes the mail application to launch
-            myIntent.PutExtra(Intent.ExtraSubject, projectName + " Project File"); // set the email subject
-            myIntent.PutExtra(Intent.ExtraStream, uri); // add the .spaniel file as email attachment
+            // Also known as the "share" intent, you should use this in an intent with startActivity() when you have some data that the user can share through 
+            // another app, such as an email app or social sharing app.
+            var myIntent = new Intent(Intent.ActionSend); 
+
+            myIntent.SetType("text/xml");
+            //myIntent.SetType("message/rfc822"); // causes the mail application to launch. If multiple applications are capable of handling mail, the user will get a list to choose from.
+
+            //myIntent.PutExtra(Intent.ExtraSubject, projectName + " Project File.spaniel"); // set the email subject
+            // 1/1/18 - for whatever reason, this is what gets used as the filename when saved to Google Drive, NOT what gets passed in.
+
+            // Set the data
+            myIntent.PutExtra(Intent.ExtraStream, uri);
+
+
             var chooser = Intent.CreateChooser(myIntent, "Send Project File");
 
+            // This fixes an error on API 22: "Calling startActivity() from outside of an Activity context requires the FLAG_ACTIVITY_NEW_TASK flag. Is this really what you want?"
+            chooser.AddFlags(ActivityFlags.NewTask);
 
-//            if (myIntent.ResolveActivity(Forms.Context.PackageManager) != null)
-//            {
-                Forms.Context.StartActivity(chooser);
-//            }
-//            else
-//            {
-//                // the intent can't resolve to any activities
-//            }
+            //if (myIntent.ResolveActivity(Forms.Context.PackageManager) != null)
+            //{
+                localContext.StartActivity(chooser);
+            //}
+            //else
+            //{
+            //    // the intent can't resolve to any activities
+            //}
         }
     }
 }
