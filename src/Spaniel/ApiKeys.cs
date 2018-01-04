29f25db266c7119147553f4c706ca8854e3eef7b
Tom Soderling <hey_tommy@icloud.com>
2018-01-04 00:11:24 -0600

    comments

--- src/Spaniel/ApiKeys.cs
+++ src/Spaniel/ApiKeys.cs
@@ -1,6 +1,10 @@
 ﻿
 namespace Spaniel
 {
+    // Changes to this file are ignored so API keys won't be checked in to github. Used command:
+    // git update-index --assume-unchanged [path to file]
+    // To undo for release branch:
+    // git update-index --no-assume-unchanged [path to file]
     public static class ApiKeys
     {
         public const string AppCenterAppSecret_iOS = "[your iOS App Center secret goes here]";
